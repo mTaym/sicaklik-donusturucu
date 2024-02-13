7811f30fc6a3f3b744e0b65a4320db7fc3039691
mete aymayan <154817485+mTaym@users.noreply.github.com>
2024-02-13 14:55:48 +0100

    Add files via upload

---
+++ mete Alıştırma 1. Sıcaklık Dönüştürücü.py
@@ -0,0 +1,16 @@
+
+celsius = ("Lutfen havanin Celsius sicaklik degerini giriniz:")
+
+celsius = float(input(celsius))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print("Havanin fahrenheit degeri :" , fahrenheit )
+
+
+
+
+
+
+
+
